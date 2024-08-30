486ec98e65100effa65fa98c50a5dc906381d5f8
xiaokai <xiaokai@kk.com>
2024-08-30 16:44:17 +0700

    bug

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import time
+import traceback
 from flask import Flask, request, jsonify
 
 from pikpak_captcha import slider_validation
@@ -9,13 +10,15 @@ app = Flask(__name__)
 def get_data():
     # 获取请求中的 JSON 数据
     url = request.args.get("url")
+    error = None
     for count in range(3):
         try:
-            token = slider_validation(url) #这里就是验证模块 用于获取url的token
+            token = slider_validation(url)  # 这里就是验证模块 用于获取url的token
             break
-        except:
+        except Exception as e:
             token = None
-            time.sleep(5)
+            time.sleep(1)
+            error = e
     if token and token != "":
         response_data = {
             'url_received': url,
@@ -27,6 +30,7 @@ def get_data():
         response_data = {
             'url_received': url,
             "code": -1,
+            'token': str(error) + str(traceback.format_exc()),
         }
         return jsonify(response_data), 400
 
